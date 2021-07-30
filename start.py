9bb7ac06187555b94782cc3d6b281fcfa30ac71f
Dean Allen <deanalle@iu.edu>
2021-07-29 17:28:12 -0700

    initial

---
+++ start.py
@@ -0,0 +1,51 @@
+from app.models.user import User, Role, Permission
+from app import create_app, db
+from flask_migrate import Migrate
+import click
+import os
+
+from dotenv import load_dotenv
+
+dotenv_path = os.path.join(os.path.dirname(__file__), ".env")
+if os.path.exists(dotenv_path):
+    print("loading dotenv")
+    load_dotenv(dotenv_path)
+
+##
+
+
+# Import models
+
+print("conff",os.getenv("FLASK_CONFIG"))
+app = create_app(os.getenv("FLASK_CONFIG") or "default")
+migrate = Migrate(app, db)
+
+
+@app.shell_context_processor
+def make_shell_context():
+    return dict(db=db, User=User, Role=Role, Permission=Permission)
+
+
+@app.cli.command()
+@click.argument("test_names", nargs=-1)
+def test(test_names):
+    """ Run unit tests """
+    import unittest
+
+    if test_names:
+        """ Run specific unit tests.
+
+        Example:
+        $ flask test tests.test_auth_api tests.test_user_model ...
+        """
+        tests = unittest.TestLoader().loadTestsFromNames(test_names)
+
+    else:
+        tests = unittest.TestLoader().discover("tests", pattern="test*.py")
+
+    result = unittest.TextTestRunner(verbosity=2).run(tests)
+    if result.wasSuccessful():
+        return 0
+
+    # Return 1 if tests failed, won't reach here if succeeded.
+    return 1
